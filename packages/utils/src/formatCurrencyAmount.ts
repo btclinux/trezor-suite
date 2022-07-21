bfa55be7095f3c4abb86bd8b3e12f97060786f6e
dahaca <dahaca17@gmail.com>
2022-07-21 13:08:51 +0200

    fixup! feat(suite): format crypto amounts based on bitcointUnits state

--- packages/utils/src/formatCurrencyAmount.ts
+++ packages/utils/src/formatCurrencyAmount.ts
@@ -1,11 +1,12 @@
-export const formatCurrencyAmount = (amount: number, locale = 'en') => {
+export const formatCurrencyAmount = (amount: number, locale = 'en'): string => {
     if (
         typeof amount !== 'number' ||
         Number.isNaN(amount) ||
         !Number.isFinite(amount) ||
-        amount > Number.MAX_SAFE_INTEGER
+        amount > Number.MAX_SAFE_INTEGER ||
+        amount < Number.MIN_SAFE_INTEGER
     ) {
-        return;
+        return '';
     }
 
     return Intl.NumberFormat(locale, { style: 'decimal' }).format(amount);
--- packages/utils/tests/formatCurrencyAmount.test.ts
+++ packages/utils/tests/formatCurrencyAmount.test.ts
@@ -10,14 +10,15 @@ describe('formatCurrencyAmount', () => {
     it('formats with cs locale', () => {
         const formattedValue = formatCurrencyAmount(123456789, 'cs');
 
-        expect(formattedValue).toStrictEqual('123 456 789');
+        expect(formattedValue).toStrictEqual('123 456 789');
     });
 
     it('fails with wrong values', () => {
-        expect(formatCurrencyAmount(NaN)).toBeNull();
-        expect(formatCurrencyAmount(Infinity)).toBeNull();
-        expect(formatCurrencyAmount(Number.MIN_SAFE_INTEGER + 1)).toBeNull();
+        expect(formatCurrencyAmount(NaN)).toStrictEqual('');
+        expect(formatCurrencyAmount(Infinity)).toStrictEqual('');
+        expect(formatCurrencyAmount(Number.MAX_SAFE_INTEGER + 1)).toStrictEqual('');
+        expect(formatCurrencyAmount(Number.MIN_SAFE_INTEGER - 1)).toStrictEqual('');
         // @ts-expect-error invalid arg
-        expect(formatCurrencyAmount('asadff')).toBeNull();
+        expect(formatCurrencyAmount('asadff')).toStrictEqual('');
     });
 });
