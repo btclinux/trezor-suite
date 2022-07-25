a4b1798ab2491da828f3debb92afe3708304ed8b
Tomáš Klíma <tomas.klim@gmail.com>
2022-07-25 17:15:59 +0200

    fix(desktop): execPath breaks argv on macOS on restart

--- packages/suite-desktop/src-electron/libs/app-utils.ts
+++ packages/suite-desktop/src-electron/libs/app-utils.ts
@@ -1,13 +1,22 @@
 import { app, RelaunchOptions } from 'electron';
 
+const isLinux = process.platform === 'linux';
+
 export const restartApp = () => {
+    const { logger } = global;
+
     const options: RelaunchOptions = {};
+
     options.args = process.argv.slice(1).concat(['--relaunch']);
-    options.execPath = process.execPath;
-    if (process.env.APPIMAGE) {
+
+    if (isLinux && process.env.APPIMAGE) {
         options.execPath = process.env.APPIMAGE;
         options.args.unshift('--appimage-extract-and-run');
+    } else if (isLinux) {
+        options.execPath = process.execPath;
     }
+    logger.info('app', `Relaunching app with ${options.args.join(', ')} arguments.`);
+
     app.relaunch(options);
     app.quit();
 };
